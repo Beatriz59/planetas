62720670eb60887be04eae381860af0040c575cf
Beatriz59 <beatrizroberto290@gmail.com>
2025-02-17 22:18:39 -0300

    Create planeta.dart

---
+++ planeta.dart
@@ -0,0 +1,16 @@
+class Planeta {
+  int id;
+  String nome;
+  double tamanho;
+  double distancia;
+  String? apelido;
+
+ //Construtor da classe Planeta
+  Planeta({
+    required this.id,
+    required this.nome,
+    required this.tamanho,
+    required this.distancia,
+    required this.apelido,
+  });
+}
